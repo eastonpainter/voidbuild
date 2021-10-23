ca20c0984ad3a1277d5fd554c788f8034e6fa13a
Easton Painter <eastonpainter@protonmail.com>
2021-10-22 22:07:21 -0500

    Fixed path issues

--- void-install.sh
+++ void-install.sh
@@ -1,18 +1,16 @@
 xbps-install -Su
-xbps-install libXft-devel libX11-devel harfbuzz-devel libXext-devel libXrender-devel libXinerama-devel vim htop xorg cparser neofetch cava tty-clock ncmpcpp mpc mpd zsh picom terminus-font xwallpaper dmenu firefox acpi make pkg-config
-echo "zsh" >> ~/.bashrc
-# Build st
+xbps-install libXft-devel libX11-devel harfbuzz-devel libXext-devel libXrender-devel libXinerama-devel vim htop xorg cparser neofetch cava tty-clock ncmpcpp mpc mpd zsh picom terminus-font xwallpaper dmenu firefox acpi make pkg-config opendoas
+cd /home/eve
+echo "zsh" >> /home/eve/.bashrc
 git clone https://github.com/siduck76/st.git
 cd st
 make install
-# Build dwm
 cd ../
 git clone https://git.suckless.org/dwm
 cd dwm
 make clean install
 touch /etc/doas.conf
 echo "permit nopass eve as root" > /etc/doas.conf
-cd ~
 su eve
 zsh
 source ~/.zsh_aliases
@@ -20,19 +18,9 @@ source ~/.zshrc
 mkdir -p pics/wallpapers
 mkdir vids notes music programs projects .scripts
 mv wall.png ~/pics/wallpapers
-mv ~/config.h ~/dwm
+doas mv ~/config.h ~/dwm
 cd ~/dwm
 doas make clean install
 cd ~
 curl -O https://raw.githubusercontent.com/dylanaraps/pfetch/master/pfetch
 mv pfetch ~/.scripts
-# Files:
-# .vim
-# .vimrc
-# .xinitrc
-# .zshrc
-# .zsh_aliases
-# .Xresources
-# config.h
-# music/
-# pics/wallpapers/
